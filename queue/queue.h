bd8100b62a5e14ae7c002d298a562cc84c511149
Peter-Chou <zhou_00009@126.com>
2018-12-11 08:48:13 +0800

    add funcion comments

--- queue/queue.h
+++ queue/queue.h
@@ -7,11 +7,22 @@ namespace datastruct {
 template <typename T>
 class Queue : private List<T> {
  public:
+  // 返回Queue已存的数量
   Rank size() const { return List<T>::size(); }
+
+  // 判断Queue是否为空
   bool empty() const { return List<T>::empty(); }
+
+  // 在Queue尾部添加元素
   void enqueue(const T& e) { this->insertAsLast(e); }
+
+  // 在Queue头部取出元素
   T dequeue() { return this->remove(this->first()); }
+
+  //查看Queue头部元素
   T& front() { return this->first()->data; }
+
+  // 函数遍历作用与Queue里的每个元素
   void traverse(std::function<void(T&)> functor) {
     return List<T>::traverse(functor);
   }
--- stack/stack.h
+++ stack/stack.h
@@ -11,11 +11,22 @@ namespace datastruct {
 template <typename T>
 class Stack : private Vector<T> {
  public:
+  // 返回Stack已存的数量
   Rank size() const { return Vector<T>::size(); }
+
+  // 判断Stack是否为空
   bool empty() const { return Vector<T>::empty(); }
+
+  // 在栈顶添加元素
   void push(const T& e) { this->insert(this->size(), e); }
+
+  // 从栈顶取出元素
   T pop() { return this->remove(this->size() - 1); }
+
+  // 查看栈顶的元素
   T& top() { return (*this)[this->size() - 1]; }
+
+  // 函数遍历作用与Stack里的每个元素
   void traverse(std::function<void(T&)> functor) {
     return Vector<T>::traverse(functor);
   }
